6d8ea4f0cf637d69162a6063516b88ba24e07228
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-05 10:16:03 -0600

    data.dart page has been updated

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -41,7 +41,37 @@ List<SliderModel> getSlides() {
   SliderModel sliderModel = new SliderModel();
 
   //1
-
   sliderModel.setImageAssetPath("assets/img1.png");
-  sliderModel.setTitle("")
+  sliderModel.setTitle("");
+  sliderModel.setDescription("Mavqut мобил иловасига хуш келибсиз");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  //2
+  sliderModel.setImageAssetPath("assets/img2.png");
+  sliderModel.setTitle("");
+  sliderModel.setDescription("Ушбу илова сизга намоз ва рўза ибодатларининг қазоларини аниқ ҳисоблаш ва тўкис адо этиш имкониятини тақдим этади.");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  //3
+  sliderModel.setImageAssetPath("assets/img3.png");
+  sliderModel.setTitle("");
+  sliderModel.setDescription("Барча ёшдаги мўмин ва муслималарнинг фарзларини тўлиқ бажаришларида Мавқут намоз ва рўза  қазоларинининг миқдори, тахминий тугатилиш вақти ва жорий статистик маълумотлар билан таъминлайди.");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  //4
+  sliderModel.setImageAssetPath("assets/img4.png");
+  sliderModel.setTitle("");
+  sliderModel.setDescription("Мусулмон аҳли фарзларимизни мукаммал адо этайлик. Aллоҳ барчамизни илмимизни зиёда қилсин!");
+  slides.add(sliderModel);
+
+  sliderModel = new SliderModel();
+
+  return slides;
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:html';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'data/data.dart';
@@ -24,6 +28,7 @@ class Home extends StatefulWidget{
 class _HomeState extends State<Home> {
 
   List<SliderModel> slides = new List<SliderModel>();
+  int currentIndex = 0;
 
   @override
   void initState() {
@@ -31,11 +36,58 @@ class _HomeState extends State<Home> {
     super.initState();
     slides = getSlides();
   }
-  
+  Widget pageIndexIndicator(bool isCurrentPage) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 2.0),
+      height: 10.0,
+      width: 10.0,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(itemBuilder: null,),
+      body: PageView.builder(
+        itemCount: slides.length,
+          onPageChanged:(val){
+          setState(() {
+            currentIndex = val;
+          });
+          },
+          itemBuilder: (context, index){
+          return SliderTitle(
+            imageAssetPart: slides[index].getImageAssetPath(),
+            title: slides[index].getTitle(),
+            description: slides[index].getDescription(),
+          );
+    }),
+      bottomSheet: currentIndex != slides.length-1 ? Container(
+        height: Platform.isIOS 70 : 50,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            InkWell(
+              onTap: (){},
+                child: Text("SKIP")
+            ),
+            Row(
+              children: <Widget>[
+                for(int i=0; i<slides.length; i++) currentIndex == i ?pageIndexIndicator(true) :pageIndexIndicator(false)
+              ],
+            ),
+            InkWell(
+                onTap: (){},
+                child: Text("SKIP")
+            ),
+          ],
+        ),
+    ) : Container(
+        child: Text(""),
+
+    )
     );
   }
 }
@@ -48,6 +100,7 @@ class SliderTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image.asset(imageAssetPart),
           SizedBox(height: 20,),
